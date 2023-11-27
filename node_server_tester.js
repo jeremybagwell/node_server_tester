08dea9ccdc33941b556c41b43265cd33b4924fbb
Jeremy Bagwell <jeremy.r.bagwell@medtronic.com>
2023-11-27 14:24:35 -0700

    clean up for demo purposes

--- node_server_tester.js
+++ node_server_tester.js
@@ -2,22 +2,34 @@
 const express = require('express');
 const app = express();
 
-const logger = require('js_logging_mnav')("node_server_tester")
+const { MNavLogger, SecStatus, SecType } = require('@juno/mnavlogging');
 
+const logger = new MNavLogger('node_test_app')
 
-// Serve the HTML file with the button to the client
 app.get('/', (req, res) => {
   res.send(`
     <html>
       <head>
         <style>
+          body {
+            text-align: center; /* Centers content of the body */
+            margin-top: 20px; /* Adds some space at the top */
+          }
           .button {
             background-color: red;
             color: white;
+            border: none;
+            padding: 10px 20px;
+            font-size: 16px;
+            border-radius: 25px;
+            cursor: pointer;
+            transition: background-color 0.3s ease;
+            margin-top: 20px; /* Adds some space above the button */
           }
         </style>
       </head>
       <body>
+        <h1>Server-side Test App</h1>
         <button id="toggle-button" class="button">Click me</button>
         <script>
           const button = document.getElementById('toggle-button');
@@ -29,7 +41,6 @@ app.get('/', (req, res) => {
               response.text().then(newColor => {
                 button.style.backgroundColor = newColor;
                 color = newColor;
-
               });
             });
           });
@@ -39,15 +50,20 @@ app.get('/', (req, res) => {
   `);
 });
 
+
 // Handle requests to toggle the button color
 let currentColor = 'red';
 app.get('/toggle-color', (req, res) => {
   if (currentColor === 'red') {
     currentColor = 'green';
-    logger.info(`currentColor = green`)
+    logger.setCorrelationId("1234ASDF")
+    logger.diagInfo(`currentColor = green`)
+    logger.diagInfo("999393", "another message")
+    logger.secInfo('someCorrId', SecType.INPV, SecStatus.PASS, "security message")
   } else {
     currentColor = 'red';
-    logger.info(`currentColor = red`)
+    logger.diagInfo(`currentColor = red`)
+    logger.secInfo('someCorrId', SecType.INPV, SecStatus.PASS, "security message")
   }
   res.send(currentColor);
 });
